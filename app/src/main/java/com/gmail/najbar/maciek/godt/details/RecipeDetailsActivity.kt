1ad5f538e5dc6ba04bfff70381f62a54e4031a99
Maciej Najbar <maciek.najbar@gmail.com>
2019-01-11 02:32:19 +0100

    Displays details for picked recipe

--- app/src/main/java/com/gmail/najbar/maciek/godt/details/RecipeDetailsActivity.kt
+++ app/src/main/java/com/gmail/najbar/maciek/godt/details/RecipeDetailsActivity.kt
@@ -1,12 +1,18 @@
 package com.gmail.najbar.maciek.godt.details
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.gmail.najbar.maciek.godt.R
 import com.gmail.najbar.maciek.godt.details.presenter.DisplayDetailsPresenter
 import com.gmail.najbar.maciek.repository.cache.DisplayDetailsGateway
 import com.gmail.najbar.maciek.usecase.DisplayDetails
 import com.gmail.najbar.maciek.usecase.DisplayDetailsImpl
+import kotlinx.android.synthetic.main.activity_recipe_details.details_recipeDescription
+import kotlinx.android.synthetic.main.activity_recipe_details.details_recipeImage
+import kotlinx.android.synthetic.main.activity_recipe_details.details_recipeIngredients
+import kotlinx.android.synthetic.main.activity_recipe_details.details_recipeTitle
 
 class RecipeDetailsActivity : AppCompatActivity(),
         RecipeDetailsContract.DisplayDetailsView {
@@ -19,10 +25,17 @@ class RecipeDetailsActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_details)
+
+        displayDetails.of(intent.getLongExtra(EXTRA_RECIPE_ID, -1))
     }
 
     override fun displayDetailsOf(recipe: DisplayDetails.Recipe) {
-
+        details_recipeTitle.text = recipe.title
+        details_recipeDescription.text = recipe.description
+        details_recipeIngredients.text = recipe.ingredients.joinToString { "* $it\n" }
+        recipe.imageUrl?.let {
+            Glide.with(this).asDrawable().load(Uri.parse(it)).into(details_recipeImage)
+        }
     }
 
     companion object {
